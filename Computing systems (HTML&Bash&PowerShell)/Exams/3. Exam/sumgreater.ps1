8d331463fbcd7cf43157a20925716d050510bfe1
Bence Széplaki <szeplakibence@gmail.com>
2021-04-29 10:25:11 +0200

    Update sumgreater.ps1

--- Computing systems (HTML&Bash&PowerShell)/Exams/3. Exam/sumgreater.ps1
+++ Computing systems (HTML&Bash&PowerShell)/Exams/3. Exam/sumgreater.ps1
@@ -1,16 +1,14 @@
-﻿$file = Get-Content $args[0]
+$file = Get-Content $args[0]
 [int]$num = $args[1]
 [int]$sum = 0
 for($i = 0; $i -lt $file.Length; $i++)
 {
     if([int]$file[$i] -gt $num)
     {
-        $file[$i]
         $sum = $sum + $file[$i]
     }
 }
 Write-Host $sum
 Write-Output $sum > tmp.txt
-Write-Output "`n">> $args[0]
 Write-Output (Get-Content tmp.txt) >> $args[0]
 Remove-Item tmp.txt
